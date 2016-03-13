8abf6a87c21e4287f82c1ebc2ece16e3aa949966
Zach Eagle <mazaxakep@gmail.com>
2016-03-13 14:49:40 +0300

    HomePage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
 import ReactTooltip from 'react-tooltip'
+import HomePage from './HomePage'
 import styles from './App.sss'
 
 @CSSModules(styles)
@@ -8,16 +9,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <div styleName='header'>
-          <div styleName='title'>
-            <img styleName='face' src='../../assets/images/sad-face.svg' />
-            ORLOVSKY
-          </div>
-        </div>
-        <div styleName='about'>
-          <div>Hey! My name is Zach Orlovsky</div>
-        </div>
-        <ReactTooltip place='bottom' />
+        <HomePage />
       </div>
     )
   }
---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import CSSModules from 'react-css-modules'
+import styles from './HomePage.sss'
+
+@CSSModules(styles)
+export default class HomePage extends Component {
+  render () {
+    return (
+      <div styleName='home'>
+        <div styleName='logo'>
+          <img styleName='face' src='../../assets/images/sad-face.svg' />
+          ORLOVSKY
+        </div>
+      </div>
+    )
+  }
+}
