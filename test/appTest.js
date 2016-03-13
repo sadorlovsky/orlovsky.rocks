df8f6858bd474eecb144a1b15eae5245976a1b74
Zach Eagle <mazaxakep@gmail.com>
2016-03-13 14:37:49 +0300

    setup tests

---
+++ test/appTest.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+import React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import App from '../src/components/App'
+
+test('toggle--off class applied by default', t => {
+  t.fail()
+})
