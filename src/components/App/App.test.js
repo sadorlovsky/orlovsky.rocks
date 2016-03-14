e78a2fdb82f326f38a63544de6d3b94672f05cba
Zach Eagle <mazaxakep@gmail.com>
2016-03-14 13:48:54 +0300

    tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,9 +2,9 @@ import React from 'react'
 import test from 'ava'
 import { shallow } from 'enzyme'
 import App from './App.js'
-import HomePage from '../HomePage/HomePage.js'
+import ProjectPage from '../ProjectPage/ProjectPage.js'
 
-test('App component renders HomePage component', t => {
+test('App component renders ProjectPage component', t => {
   const wrapper = shallow(<App />)
-  t.is(wrapper.find(HomePage).length, 1)
+  t.is(wrapper.find(ProjectPage).length, 1)
 })
