3483738cd06e159d02b74aecae381925e8be51e6
Zach Eagle <mazaxakep@gmail.com>
2016-03-16 19:03:38 +0300

    Add Blog component

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import App from './components/App/App.js'
 import ProjectPage from './components/ProjectPage/ProjectPage.js'
 import HomePage from './components/HomePage/HomePage.js'
 import AboutPage from './components/AboutPage/AboutPage.js'
+import Blog from './components/Blog/Blog.js'
 
 module.exports = (
   <Route path='/' component={App}>
     <IndexRoute component={HomePage} />
     <Route path='about' component={AboutPage} />
+    <Route path='blog' component={Blog} />
     <Route path='projects' component={ProjectPage} />
   </Route>
 )
