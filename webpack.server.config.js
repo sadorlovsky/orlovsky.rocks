4e65f9816ed71ac8a2b794025eda9a32eb3b2804
Zach Eagle <mazaxakep@gmail.com>
2016-03-17 14:18:56 +0300

    fix path

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -36,7 +36,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|svg)$/,
-        loader: 'file-loader?context=static&name=/static/images/[name].[ext]'
+        loader: 'file-loader?name=static/images/[name].[ext]'
       }
     ]
   },
