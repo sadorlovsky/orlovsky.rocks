18a5d6a501f6035896c48272b07be656c6b5e535
Zach Eagle <mazaxakep@gmail.com>
2016-03-28 02:37:25 +0300

    return json from api

--- server/api/projects.js
+++ server/api/projects.js
@@ -1,13 +1,37 @@
 import express from 'express'
+import { find } from 'lodash'
 
 const router = express.Router()
 
+const projects = [
+  {
+    id: 1,
+    title: 'dockerps'
+  },
+  {
+    id: 2,
+    title: 'currency converter'
+  },
+  {
+    id: 3,
+    title: 'codestyle'
+  },
+  {
+    id: 4,
+    title: 'sayhey'
+  },
+  {
+    id: 5,
+    title: 'inteh-info.ru telegram bot'
+  }
+]
+
 router.get('/', (req, res) => {
-  res.send('all projects')
+  res.json(projects)
 })
 
 router.get('/:id', (req, res) => {
-  res.send(`project #${req.params.id}`)
+  res.json(find(projects, ['id', parseInt(req.params.id)]))
 })
 
 export default router
