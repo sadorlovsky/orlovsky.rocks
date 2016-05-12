31230c156df77a44d26c5f1aed62cc7ecc0da67d
Zach Orlovsky <sadorlovsky@gmail.com>
2016-05-12 17:10:40 +0300

    remove projectsApi middleware

--- server/dev.js
+++ server/dev.js
@@ -4,7 +4,6 @@ import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../webpack/webpack.dev.config'
-import projectsApi from './api/projects'
 import blogApi from './api/blog'
 
 const app = express()
@@ -17,7 +16,6 @@ const hotMiddleware = webpackHotMiddleware(compiler)
 app.use(devMiddleware)
 app.use(hotMiddleware)
 
-app.use('/api/projects', projectsApi)
 app.use('/api/blog', blogApi)
 
 app.get('*', (_, res) => {
